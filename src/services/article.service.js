3ce09af1d1916f5d27a412b36cd592869a1d0c65
= <=>
2026-02-26 13:13:47 +0530

    added api.js

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -1,10 +1,10 @@
 import API from "../api/axios";
 
-export const getArticles = (params) =>
+export const getArticles = (params) => {
+  console.log("API URL:", import.meta.env.VITE_API_URL);
   API.get("/articles", { params });
+};
 
-export const createArticle = (data) =>
-  API.post("/articles", data);
+export const createArticle = (data) => API.post("/articles", data);
 
-export const aiAssist = (data) =>
-  API.post("/articles/ai/assist", data);
+export const aiAssist = (data) => API.post("/articles/ai/assist", data);
