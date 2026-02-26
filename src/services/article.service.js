e4fe9de2755f578df2b93b6acb54755ded4ffa1f
= <=>
2026-02-26 13:30:16 +0530

    changed get articles api

--- src/services/article.service.js
+++ src/services/article.service.js
@@ -1,9 +1,7 @@
 import API from "../api/axios";
 
-export const getArticles = (params) => {
-  console.log("API URL:", import.meta.env.VITE_API_URL);
+export const getArticles = (params) =>
   API.get("/articles", { params });
-};
 
 export const createArticle = (data) => API.post("/articles", data);
 
